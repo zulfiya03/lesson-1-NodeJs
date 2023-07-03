677cd8fabe760d21bf15d54a95fd1b759c5f2fab
Zulfiya <zulfiyaabdullaeva03@gmail.com>
2023-07-03 22:52:42 +0500

    next commit

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 const prompt = require('prompt-sync')()
 const fs = require('fs')
-const age = prompt('Yoshingizni kiriting: ')
 
-console.log(age)
+fs.readFile(__dirname + "/some.txt", 'utf-8', (err, data) => {
+    if(err){
+        console.log('Error:',err)
+    }
+    console.log(data);
+})
+
+fs.writeFile(__dirname + '/some.txt','Typescript', 'utf-8', (err) => {
+    if(err) {
+        console.log('Error:', err);
+    }
+})
+
+// const age = prompt('Yoshingizni kiriting: ')
+
+// console.log(age)
