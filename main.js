855a0c30fc09d98c648dc921a1cc748aa49a26f1
Zulfiya <zulfiyaabdullaeva03@gmail.com>
2023-07-05 14:22:57 +0500

    first commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')()
 const fs = require('fs')
 
-fs.readFile(__dirname + "/some.json", 'utf-8', (err, data) => {
+fs.readFile(__dirname + "/some.txt", 'utf-8', (err, data) => {
     if(err){
         console.log('Error:',err)
     }
@@ -12,7 +12,7 @@ const obj = {
     key: value
 }
 
-fs.writeFile(__dirname + '/some.json','Typescript', 'utf-8', (err) => {
+fs.writeFile(__dirname + '/some.txt','Typescript', 'utf-8', (err) => {
     if(err) {
         console.log('Error:', err);
     }
